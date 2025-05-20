21eea732e3e0e4eb42674fbc545306acaa0ffede
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-20 21:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getFormvalue(e) {
     //Write your code here
 	let fname= document.getElementById("fname");
 	let lname= document.getElementById("lname");
-	let btn=document.querySelector("btn");
+	let btn=document.getElementByID("btn");
 	btn.addEventListener("onclick",(e)=>{
 		
 		alert(`${fname.value}`);
