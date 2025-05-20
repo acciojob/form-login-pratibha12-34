630dc585584828723e874dcd668a18fba6cbbb06
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-20 21:25:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
-}
+	let form =document.querySelector("form");
+	form.addEventListener("submit",(e)=>{
+		let input= document.querySelector("input");
+		alert(`${input.value}`);
+	})
