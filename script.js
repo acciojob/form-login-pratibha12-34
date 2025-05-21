9dfb945c7d569234cc73bedd8a791a15b96d21ac
Pratibha Agrawal <125815236+pratibha12-34@users.noreply.github.com>
2025-05-21 12:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function getFormvalue(e) {
     //Write your code here
 	let fname= document.getElementById("fname");
 	let lname= document.getElementById("lname");
-	let btn= document.getElementByID("btn");
-	btn.addEventListener("onclick",(e)=>{
-		
-		alert("bygyhvh");
+	let form= document.getElementById("form1");
+	form.addEventListener("submit",(e)=>{
+		e.preventDefault();
+		alert(fname.value+lname.value);
 		
 	})}
